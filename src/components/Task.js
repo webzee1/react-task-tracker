5c20a925d6b659204f89b1a98493b3a354741c1d
Zaryab Haider <haiderzaryab71@gmail.com>
2021-01-24 03:52:05 +0500

    Delete Tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,10 +2,12 @@
 import { FaTimes } from 'react-icons/fa'
 
 // props from Tasks.js
-const Task = ({task}) => {
+const Task = ({task , onDelete}) => {
     return (
         <div className="task">
-           <h3> {task.text} <FaTimes style={{color: 'red' , cursor: 'pointer'}}/></h3>
+           <h3> {task.text} 
+           <FaTimes style={{color: 'red' , cursor: 'pointer'}} onClick={() => onDelete (task.id)}/>
+           </h3>
            <p> {task.day} </p>
         </div>
     )
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Task from './Task'
 
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks , onDelete }) => {
 
     return (
         <>
           {tasks.map((task) => (
-          <Task key={task.id} task={task}></Task>
+          <Task key={task.id} task={task} onDelete={onDelete}></Task>
           ))}  
         </>
     )
