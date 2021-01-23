a65b3e4f3be302b45ae05833bf5e7e9cf608c766
Zaryab Haider <haiderzaryab71@gmail.com>
2021-01-24 03:27:18 +0500

    Cross Icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,11 @@
-import React from 'react'
+//Cross icon from React
+import { FaTimes } from 'react-icons/fa'
 
 // props from Tasks.js
 const Task = ({task}) => {
     return (
         <div className="task">
-           <h3> {task.text} </h3>
+           <h3> {task.text} <FaTimes style={{color: 'red' , cursor: 'pointer'}}/></h3>
            <p> {task.day} </p>
         </div>
     )
